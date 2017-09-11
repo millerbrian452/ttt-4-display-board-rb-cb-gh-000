78fa218afc4189dd18531033164dd1fab32250a2
Brian Miller <jwino0004@mail.ct.edu>
2017-09-11 21:28:21 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[3]} ",
+  puts " #{board[0]} | #{board[1]} | #{board[2]} ",
        "-----------",
-       " #{board[4]} | #{board[5]} | #{board[6]} ",
+       " #{board[3]} | #{board[4]} | #{board[5]} ",
        "-----------",
-       " #{board[7]} |#{board[8]} | #{board[9]} "
+       " #{board[6]} |#{board[7]} | #{board[8]} "
      end
