0d22a0f4fb72c2f40949f6a2e3230dfe8755bcac
Brian Miller <jwino0004@mail.ct.edu>
2017-09-11 21:26:33 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 board = ["O", " ", " ", "O", " ", " ", "O", " ", " "]
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} |#{board[3]} ",
+  puts " #{board[0]} | #{board[1]} | #{board[3]} ",
        "-----------",
        " #{board[4]} | #{board[5]} | #{board[6]} ",
        "-----------",
-       " #{board[7]} |#{board[8]} |#{board[9]} "
+       " #{board[7]} |#{board[8]} | #{board[9]} "
      end
